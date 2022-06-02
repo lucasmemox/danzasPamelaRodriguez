da6e602bdc1ce4a342ead9d0a23259d757566289
lbentancour <lbentancour@cent.gov.ar>
2022-06-02 15:07:39 -0300

    agrego las tarjetas creadas en js

--- js/danzas.js
+++ js/danzas.js
@@ -1,31 +1,55 @@
 class Danzas {
-    constructor(estilos, descripcion, imagen, costo) {
-        this.estilos = estilos
-        this.descripcion = descripcion
-        this.imagen = imagen
-        this.costo = costo
-    }
+  constructor(estilos, imagen, costo) {
+    this.estilos = estilos;
+    this.imagen = imagen;
+    this.costo = costo;
+  }
 }
 
-const danzas1 = new Monitor('Clásico', 'Horarios: 17:00 a 21:00 de Lunes a Viernes', './imagenes/oferta/clasico.png', 3000)
-const danzas2 = new Monitor('Folclore Tradicional', 'Horarios: 17:00 a 21:00 de Lunes a Viernes', './imagenes/oferta/folclore.png', 5000)
-const danzas3 = new Monitor('Danzas Españolas', 'Horarios: 17:00 a 21:00 de Lunes a Viernes', './imagenes/oferta/español.png', 3500)
-const danzas4 = new Monitor('Ritmos Libres', 'Horarios: 17:00 a 21:00 de Lunes a Viernes', './imagenes/oferta/libres.png', 3500)
-const danzas5 = new Monitor('Jazz', 'Horarios: 17:00 a 21:00 de Lunes a Viernes', './imagenes/oferta/Jazz.png', 3700)
-const danzas6 = new Monitor('Entrenamiento Funcional', 'Horarios: 17:00 a 21:00 de Lunes a Viernes', './imagenes/oferta/funcional.png', 4000)
+const danzas1 = new Danzas(
+  "Clásico",
+  "./imagenes/oferta/clasico.png",
+  3000
+);
+const danzas2 = new Danzas(
+  "Folclore Tradicional",
+  "./imagenes/oferta/folclore.png",
+  5000
+);
+const danzas3 = new Danzas(
+  "Danzas Españolas",
+  "Horarios: 17:00 a 21:00 de Lunes a Viernes",
+  "./imagenes/oferta/español.png",
+  3500
+);
+const danzas4 = new Danzas(
+  "Ritmos Libres",
+  "./imagenes/oferta/libres.png",
+  3500
+);
+const danzas5 = new Danzas(
+  "Jazz",
+  "./imagenes/oferta/Jazz.png",
+  3700
+);
+const danzas6 = new Danzas(
+  "Entrenamiento Funcional",
+  "./imagenes/oferta/funcional.png",
+  4000
+);
 
-const listado = [danzas1, danzas2, danzas3, danzas4, danzas5, danzas6]
-const cardContainer = document.getElementById('cardContainer')
+const listado = [danzas1, danzas2, danzas3, danzas4, danzas5, danzas6];
+const cardContainer = document.getElementById("contenedor-principal");
 
 listado.forEach((listado) => {
-    const card = document.createElement('div')
-    card.className = 'card'
-    card.innerHTML = `
-            <h3 class="cardTitle"> Monitor ${listado.estilos} </h3>
-            <img src="${listado.descripcion}" class="cardImg">
-            <p class="cardDesc"> ${listado.imagen} Pulgadas</p>
-            <span class="cardPrice"> $${listado.costo} </span>
-            <button class="buttonCTA"> Agregar al Carrito </button>
-        `
-    cardContainer.append(card)
-})
+  const tarjeta = document.createElement("div");
+  tarjeta.className = "caja";
+  tarjeta.innerHTML = `
+            <h3 class="cajaTitulo">${listado.estilos}</h3>
+            <img src="${listado.imagen}" class="img-clases">
+            <span class="cajaCosto">$${listado.costo}</span>
+            <button class="botonCRR"> Agregar al Carrito </button>
+        `;
+  cardContainer.append(tarjeta);
+});
+

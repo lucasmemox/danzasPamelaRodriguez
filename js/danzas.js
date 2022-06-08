c6ccc21d518c54d809f1c32790385b46b6bbd002
lbentancour <lbentancour@cent.gov.ar>
2022-06-08 17:08:40 -0300

    empiezo a modificar el carrito para salvar el error

--- js/danzas.js
+++ js/danzas.js
@@ -23,42 +23,68 @@ const listado = [danzas1, danzas2, danzas3, danzas4, danzas5, danzas6];
 let carrito = [];
 
 //Aca tomo el id de la clase padre donde voy a mostrar las clases 
-const cardContainer = document.getElementById("contenedor-principal");
+// const cardContainer = document.getElementById("contenedor-principal");
+const cardContainer = document.querySelector('#contenedor-principal');
 
 //Genero las clases con el for each
-listado.forEach((listado) => {
+listado.forEach((tarjetas) => {
   const tarjeta = document.createElement("div");
   tarjeta.className = "caja";
   tarjeta.innerHTML = `
             
             <div class="img-contenedor">
-            <h3 class="tituloCaja">${listado.estilos} <br>
-                                   Cuota $${listado.costo}</h3>
-            <img src="${listado.imagen}" class="img-clases">
-            <button id-clase="${listado.id}" class="botonCRR"><span class="crr">Agregar</span></button>
+            <h3 class="tituloCaja">${tarjetas.estilos} <br>
+                                   Cuota $${tarjetas.costo}</h3>
+            <img src="${tarjetas.imagen}" class="img-clases">
+            <button id-clase="${tarjetas.id}" class="botonCRR"><span class="crr">Agregar</span></button>
             </div>
         `;
   cardContainer.append(tarjeta);
 });
+console.log(listado);
 
+const crrseleccion = document.querySelector('#crrseleccion');
+
+const imprimirCarrito = () => {
+  crrseleccion.innerHTML = ''
+    carrito.forEach((tarjetas) => {
+        const cartRow = document.createElement('div')
+        cartRow.className = 'cartRow'
+        cartRow.innerHTML = `
+        <div class="cartImg">
+        <img src="${tarjetas.imagen}">
+        </div>
+        <div class="cartTitle"><span> Monitor ${tarjetas.estilos}</span></div>
+        <div class="cartDesc"><span> ${tarjetas.id} Pulgadas</span></div>
+        <div class="cartPrice"><span> $${tarjetas.costo}</span></div>
+        `
+        crrseleccion.append(cartRow)
+    })
+} 
 
 // Función para agregar clase e identificar cual es la seleccionada. 
 const agregarDanza = (e) => {
   // Con target accedo  a la etiqueta del button y con getAttribute accedemos al atributo
   const claseElegida = e.target.getAttribute('id-clase')
-  // Una vez que tenemos el valor de referencia que guardamos en el boton hacemos una busqueda (find)
-  const ingresos = listado.find((ingresos) => ingresos.id ==  claseElegida)
+  // Una vez que tenemos el valor de referencia que guardamos en el botón hacemos una búsqueda (find)
+  const listado = listado.find((tarjetas) => tarjetas.id ==  claseElegida)
   // Una vez tenemos todo el objeto, lo enviamos al carrito
-  carrito.push(ingresos)
-  console.log(carrito)
+  carrito.push(listado)
+  imprimirCarrito()
+  localStorage.setItem('carrito', JSON.stringify(carrito))
 }
 
 // Accedo al botón a traves de la clase botonCRR y le agregamos el evento click
 const btnComprar = document.querySelectorAll('.botonCRR')
 btnComprar.forEach((btnCompra) => {
     btnCompra.addEventListener('click', agregarDanza)
-    console.log(btnComprar)
 })
+
+// Al cargar la pagina, verifico que exista algo guardado en el carrito y lo imprimo
+if (localStorage.getItem('carrito')) {
+  carrito = JSON.parse(localStorage.getItem('carrito'))
+  imprimirCarrito()
+}
 
 // FUNCIONES
 
@@ -71,115 +97,115 @@ btnComprar.forEach((btnCompra) => {
 // let nombre = prompt("Ingrese su nombre: ");
 // let curso = prompt("Ingrese la letra del curso: ").toUpperCase();
 
-let tipo = !isNaN(nombre);
-console.log(tipo);
+// let tipo = !isNaN(nombre);
+// console.log(tipo);
 
-if (tipo == false) {
-  switch (curso) {
-    case "F":
-      let descuentof = descuento("F");
+// if (tipo == false) {
+//   switch (curso) {
+//     case "F":
+//       let descuentof = descuento("F");
 
-      alert(
-        nombre +
-          " El Profesorador de Folklore cuesta 5000 y con beca " +
-          descuentof
-      );
-      break;
-    case "E":
-      let descuentoe = descuento("E");
-      alert(
-        nombre +
-          " El Profesorado de Español cuesta 3500 y con beca " +
-          descuentoe
-      );
-      break;
-    case "J":
-      let descuentoj = descuento("J");
-      alert(
-        nombre + " El Profesorado de Jazz cuesta 3700 y con beca " + descuentoj
-      );
-      break;
-    case "J":
-      let descuentol = descuento("L");
-      alert(
-        nombre +
-          " El Profesorado de Ritmos Libres cuesta 3500 y con beca " +
-          descuentol
-      );
-      break;
-    case "J":
-      let descuentofu = descuento("FU");
-      alert(
-        nombre +
-          " Entrenamiento funcional cuesta 4000 con descuento " +
-          descuentofu
-      );
-      break;
-    case "J":
-      let descuentoc = descuento("C");
-      alert(
-        nombre +
-          " El Profesorado de Danzas Clasicas cuesta 3000 y con beca " +
-          descuentoc
-      );
-      break;
-    default:
-      alert("No es un curso valido");
-      break;
-  }
-} else {
-  alert("No es un nombre valido");
-}
+//       alert(
+//         nombre +
+//           " El Profesorador de Folklore cuesta 5000 y con beca " +
+//           descuentof
+//       );
+//       break;
+//     case "E":
+//       let descuentoe = descuento("E");
+//       alert(
+//         nombre +
+//           " El Profesorado de Español cuesta 3500 y con beca " +
+//           descuentoe
+//       );
+//       break;
+//     case "J":
+//       let descuentoj = descuento("J");
+//       alert(
+//         nombre + " El Profesorado de Jazz cuesta 3700 y con beca " + descuentoj
+//       );
+//       break;
+//     case "J":
+//       let descuentol = descuento("L");
+//       alert(
+//         nombre +
+//           " El Profesorado de Ritmos Libres cuesta 3500 y con beca " +
+//           descuentol
+//       );
+//       break;
+//     case "J":
+//       let descuentofu = descuento("FU");
+//       alert(
+//         nombre +
+//           " Entrenamiento funcional cuesta 4000 con descuento " +
+//           descuentofu
+//       );
+//       break;
+//     case "J":
+//       let descuentoc = descuento("C");
+//       alert(
+//         nombre +
+//           " El Profesorado de Danzas Clasicas cuesta 3000 y con beca " +
+//           descuentoc
+//       );
+//       break;
+//     default:
+//       alert("No es un curso valido");
+//       break;
+//   }
+// } else {
+//   alert("No es un nombre valido");
+// }
 
-function descuento(curso) {
-  if (curso == "F") {
-    let monto = 5000;
-    let descuento = 15;
+// function descuento(curso) {
+//   if (curso == "F") {
+//     let monto = 5000;
+//     let descuento = 15;
 
-    let montodescontado = (monto * descuento) / 100;
+//     let montodescontado = (monto * descuento) / 100;
 
-    let total = monto - montodescontado;
+//     let total = monto - montodescontado;
 
-    return total;
-  } else if (curso == "J") {
-    let monto = 3700;
-    let descuento = 10;
+//     return total;
+//   } else if (curso == "J") {
+//     let monto = 3700;
+//     let descuento = 10;
 
-    let montodescontado = (monto * descuento) / 100;
+//     let montodescontado = (monto * descuento) / 100;
 
-    let total = monto - montodescontado;
-    return total;
-  } else if (curso == "E") {
-    let monto = 3500;
-    let descuento = 16;
+//     let total = monto - montodescontado;
+//     return total;
+//   } else if (curso == "E") {
+//     let monto = 3500;
+//     let descuento = 16;
 
-    let montodescontado = (monto * descuento) / 100;
+//     let montodescontado = (monto * descuento) / 100;
 
-    let total = monto - montodescontado;
-    return total;
-  } else if (curso == "C") {
-    let monto = 3000;
-    let descuento = 20;
+//     let total = monto - montodescontado;
+//     return total;
+//   } else if (curso == "C") {
+//     let monto = 3000;
+//     let descuento = 20;
 
-    let montodescontado = (monto * descuento) / 100;
+//     let montodescontado = (monto * descuento) / 100;
 
-    let total = monto - montodescontado;
-    return total;
-  } else if (curso == "L") {
-    let monto = 3500;
-    let descuento = 20;
+//     let total = monto - montodescontado;
+//     return total;
+//   } else if (curso == "L") {
+//     let monto = 3500;
+//     let descuento = 20;
 
-    let montodescontado = (monto * descuento) / 100;
+//     let montodescontado = (monto * descuento) / 100;
 
-    let total = monto - montodescontado;
-    return total;
-  } else if (curso == "FU") {
-    let monto = 4000;
-    let descuento = 10;
+//     let total = monto - montodescontado;
+//     return total;
+//   } else if (curso == "FU") {
+//     let monto = 4000;
+//     let descuento = 10;
 
-    let montodescontado = (monto * descuento) / 100;
+//     let montodescontado = (monto * descuento) / 100;
 
-    let total = monto - montodescontado;
-    return total;
-  }
-}
+//     let total = monto - montodescontado;
+//     return total;
+//   }
+// }

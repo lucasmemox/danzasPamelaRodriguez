06f202d548606409137f7a93c5b2f30b671f4ab0
lbentancour <lbentancour@cent.gov.ar>
2022-06-17 01:01:06 -0300

    agrego cabecera

--- js/danzas.js
+++ js/danzas.js
@@ -9,12 +9,32 @@ class Danzas {
 }
 
 //Defino las clases ofrecidas por la academia
-const clasico = new Danzas(1,"Clásico", "./imagenes/oferta/clasico.png", 3000);
-const folclore = new Danzas(2,"Folclore Tradicional","./imagenes/oferta/folclore.png",5000);
-const espaniol = new Danzas(3,"Danzas Españolas","./imagenes/oferta/español.png",3500);
-const rlibres = new Danzas(4,"Ritmos Libres","./imagenes/oferta/libres.png",3500);
-const djazz = new Danzas(5,"Jazz", "./imagenes/oferta/Jazz.png", 3700);
-const efuncional = new Danzas(6,"Entrenamiento Funcional","./imagenes/oferta/funcional.png",4000);
+const clasico = new Danzas(1, "Clásico", "./imagenes/oferta/clasico.png", 3000);
+const folclore = new Danzas(
+  2,
+  "Folclore Tradicional",
+  "./imagenes/oferta/folclore.png",
+  5000
+);
+const espaniol = new Danzas(
+  3,
+  "Danzas Españolas",
+  "./imagenes/oferta/español.png",
+  3500
+);
+const rlibres = new Danzas(
+  4,
+  "Ritmos Libres",
+  "./imagenes/oferta/libres.png",
+  3500
+);
+const djazz = new Danzas(5, "Jazz", "./imagenes/oferta/Jazz.png", 3700);
+const efuncional = new Danzas(
+  6,
+  "Entrenamiento Funcional",
+  "./imagenes/oferta/funcional.png",
+  4000
+);
 
 //Creo el array de clases de danza
 const listado = [clasico, folclore, espaniol, rlibres, djazz, efuncional];
@@ -22,11 +42,13 @@ const listado = [clasico, folclore, espaniol, rlibres, djazz, efuncional];
 //Creo el carrito vacio -  es un array
 let carrito = [];
 
-//Aca tomo el id de la clase padre donde voy a mostrar las clases 
+//Aca tomo el id de la clase padre donde voy a mostrar las clases
 // const cardContainer = document.getElementById("contenedor-principal");
-const cardContainer = document.querySelector('#contenedor-principal');
+const cardContainer = document.querySelector("#contenedor-principal");
 
-const crrseleccion = document.querySelector('#crrseleccion');
+const crrseleccion = document.querySelector("#crrseleccion");
+
+const crrcabecera = document.querySelector("#crrcabecera");
 
 //Genero las clases con el for each
 listado.forEach((tarjetas) => {
@@ -45,63 +67,121 @@ listado.forEach((tarjetas) => {
   console.log(tarjetas);
 });
 
-const imprimirCarrito = () => {
-  crrseleccion.innerHTML = ''
-    carrito.forEach((tarjetas) => {
-        const cartRow = document.createElement('div')
-        cartRow.className = 'cartRow'
-        cartRow.innerHTML = `
-        <div class="cartImagen">
-             <img class="tarjetaImagen" src="${tarjetas.imagen}">
-        </div>
-        <div class="cartTitulo"><span>${tarjetas.estilos}</span></div>
-        <div class="cartMonto"><span>$${tarjetas.costo}</span></div>  
-        <div class="cartMonto"><span>${tarjetas.id}</span></div>  
-        `
-        crrseleccion.append(cartRow)
-    })
-} 
+// const imprimirCabecera = () => {
+//   console.log("Entre ACA");
+//   const cartCabecera = document.createElement("div");
+//   cartCabecera.className = "cartCabecera";
+//   console.log(cartCabecera);
+//   cartCabecera.innerHTML = `
+//         <div class="cartImagen">
+//              <span class="tarjetaImagen"> Descripción</span>
+//         </div>
+//         <div class="cartTitulo"><span>Estilo</span></div>
+//         <div class="cartMonto"><span>Costo</span></div>
+//         <div class="cartMonto"><span>Orden</span></div>
+//         `;
+//   crrcabecera.append(cartCabecera);
+// };
 
-// Función para agregar clase e identificar cual es la seleccionada. 
+const imprimirCarrito = () => {
+  crrseleccion.innerHTML = "";
+  console.log("Entre ACA");
+  const cartCabecera = document.createElement("div");
+  cartCabecera.className = "cartCabecera";
+  cartCabecera.innerHTML = `
+       <div class="cartCabecera">
+        <div class="cartImagen">
+             <span class="tarjetaImagen"> Descripción</span>
+        </div>
+        <div class="cartTitulo"><span>Estilo</span></div>
+        <div class="cartMonto"><span>Costo</span></div>  
+        <div class="cartMonto"><span>Orden</span></div>
+      </div>  
+  `;
+  crrseleccion.append(cartCabecera);
+
+  // crrseleccion.innerHTML = "";
+  carrito.forEach((tarjetas) => {
+    const cartRow = document.createElement("div");
+    cartRow.className = "cartRow";
+    cartRow.innerHTML = `
+    
+          <div class="cartImagen">
+             <img class="tarjetaImagen" src="${tarjetas.imagen}">
+          </div>
+        
+          <div class="cartTitulo"><span>${tarjetas.estilos}</span></div>
+       
+          <div class="cartMonto"><span>$${tarjetas.costo}</span></div>
+       
+          <div class="cartMonto"><span>${tarjetas.id}</span></div>
+        
+        `;
+    crrseleccion.append(cartRow);
+  });
+};
+
+const imprimirCarrito2 = () => {
+  crrseleccion.innerHTML = "";
+  carrito.forEach((tarjetas) => {
+    const cartRow = document.createElement("div");
+    cartRow.className = "cartRow";
+    cartRow.innerHTML = `
+    
+          <div class="cartImagen">
+             <img class="tarjetaImagen" src="${tarjetas.imagen}">
+          </div>
+        
+          <div class="cartTitulo"><span>${tarjetas.estilos}</span></div>
+       
+          <div class="cartMonto"><span>$${tarjetas.costo}</span></div>
+       
+          <div class="cartMonto"><span>${tarjetas.id}</span></div>
+        
+        `;
+    crrseleccion.append(cartRow);
+  });
+};
+
+// Función para agregar clase e identificar cual es la seleccionada.
 const agregarDanza = (e) => {
   // Con target accedo  a la etiqueta del button y con getAttribute accedemos al atributo
-  const claseElegida = e.target.closest('.botonCRR').getAttribute('id-danza')
+  const claseElegida = e.target.closest(".botonCRR").getAttribute("id-danza");
   // Una vez que tenemos el valor de referencia que guardamos en el botón hacemos una búsqueda (find)
-  const agregarRender = listado.find((tarjetas) => tarjetas.id ==  claseElegida)
+  const agregarRender = listado.find((tarjetas) => tarjetas.id == claseElegida);
   // Una vez tenemos todo el objeto, lo enviamos al carrito
-  console.log(agregarRender)
+  console.log(agregarRender);
   //controlarCarrito()
-  carrito.push(agregarRender)
-  imprimirCarrito()
-  localStorage.setItem('carrito', JSON.stringify(carrito))
-}
+
+  carrito.push(agregarRender);
+  imprimirCarrito();
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
 
 // Accedo al botón a traves de la clase botonCRR y le agregamos el evento click
-const btnComprar = document.querySelectorAll('.botonCRR')
+const btnComprar = document.querySelectorAll(".botonCRR");
 btnComprar.forEach((btnCompra) => {
-    btnCompra.addEventListener('click', agregarDanza)
-})
+  btnCompra.addEventListener("click", agregarDanza);
+});
 
 // Al cargar la pagina, verifico que exista algo guardado en el carrito y lo imprimo
-if (localStorage.getItem('carrito')) {
-  carrito = JSON.parse(localStorage.getItem('carrito'))
-  imprimirCarrito()
+if (localStorage.getItem("carrito")) {
+  carrito = JSON.parse(localStorage.getItem("carrito"));
+  imprimirCarrito();
 }
 
 // Botón de Vaciar Carrito
 
 const vaciarCarrito = () => {
-  if (localStorage.getItem('carrito')) {
-      localStorage.removeItem('carrito')
+  if (localStorage.getItem("carrito")) {
+    localStorage.removeItem("carrito");
   }
-  carrito = []
-  imprimirCarrito()
-}
+  carrito = [];
+  imprimirCarrito2();
+};
 
-const vaciarCarritoBtn = document.querySelector('#vaciarCarrito')
-vaciarCarritoBtn.addEventListener('click', vaciarCarrito)
-
-
+const vaciarCarritoBtn = document.querySelector("#vaciarCarrito");
+vaciarCarritoBtn.addEventListener("click", vaciarCarrito);
 
 // FUNCIONES
 
